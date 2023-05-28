e3d44448c956055a33d83d21070a93d618d2c22f
Dor <dor.shacham@gmail.com>
2023-05-28 16:32:20 +0300

    meeting with Eyal

--- solver/final_version/main.py
+++ solver/final_version/main.py
@@ -1,4 +1,5 @@
 from SHGCWE import *
 
-A = SHGCWE(check_sol = True, draw_sol = True,N=1)
+crystal_coef = {"max_mode1": 1, "max_mode2":0, "real_coef":np.array([1]),"img_coef":np.array([0])}
+A = SHGCWE(check_sol = True,crystal_coef=crystal_coef, draw_sol = True,N=1)
 A.solve()
